d1aed859097f2281a3bff499b17e93de71245c3f
AceldamA <lord-aceldama@users.noreply.github.com>
2020-06-27 21:28:10 +0100

    Update ig-monitor.sh

--- ig-monitor.sh
+++ ig-monitor.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 function get_deets() {
-    html="$(wget -q -O - "https://www.instagram.com/$1" | grep -o -P 'sharedData = .*</script>' )"
+    local html="$(wget -q -O - "https://www.instagram.com/$1" | grep -o -P 'sharedData = .*</script>' )"
+    echo "$html"
+}
 
+function digest_json() {
+    #-- To Do
+    local html="$1"
+
+    #-- strip random strings
     if echo -n "$html" | grep -q "nonce"; then
         html="$( echo -n "$html" | grep -o -P '"viewer.*nonce"' )"
     fi
@@ -13,30 +20,48 @@ function get_deets() {
     echo "$html"
 }
 
+function get_hash() {
+    local html="$( digest_json "$1" )"
+
+    #-- return MD5
+    echo -n "$html" | md5sum
+}
+
+function archive_org {
+    echo "submitting to archive.org..."
+    wget -q -O - "https://web.archive.org/save/https://www.instagram.com/$profile/"
+    echo done!
+}
+
 
 #-- Init
 profile="$1"
 html="$( get_deets "$profile" )"
-last_hash="$( echo -n "$html" | md5sum )"
+last_hash="" #"$( $get_hash "$html" )"
 focus_on_me=0
 
 #-- Monitor loop
 while true; do
     #-- Compare hashes
-    hash="$( echo -n "$html" | md5sum )"
+    hash="$( get_hash "$html" )"
     if [ "$hash" != "$last_hash" ]; then
-        echo "changed: submitting to iwbm..."
-        wget -q -O - "https://web.archive.org/save/https://www.instagram.com/$profile/"
-        echo done!
         focus_on_me=30
-    else
-        echo "unchanged, waiting..."
+        last_hash="$hash"
+        echo "changed: $( date )"
+        echo "$html" >> ig_changes.txt
+        echo "$( digest_json "$html" )"
     fi
 
-    #-- Flood protection delay
-    if [ $focus_on_me -gt 0 ]; then
-        sleep 20 #-- wait 20 seconds
+    #-- Flood delay
+    if [[ $focus_on_me -gt 0 ]]; then
+        echo -ne " \runchanged, focused...  ($focus_on_me)"
+        focus_on_me=$(( focus_on_me - 1 ))
+        sleep 10 #-- wait N seconds
+        if [[ $focus_on_me -eq 0 ]]; then
+            echo ""
+        fi
     else
+        echo "unchanged, waiting..."
         sleep "$(( 60 * 5 ))"  #-- 5 mins
     fi
 
